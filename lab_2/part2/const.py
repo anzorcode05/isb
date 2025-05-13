bf11d51914f167d348197e0221a556acf435d8aa
Anzor Magomedagaev <anzor.m13@gmail.com>
2025-05-13 23:24:13 +0400

    final version task2

---
+++ lab_2/part2/const.py
@@ -0,0 +1,9 @@
+
+cpp_sequence_txt = "../part1/sequenceCPP.txt"
+
+java_sequence_txt = "../part1/sequenceJAVA.txt"
+
+res_test_cpp = "res_test_cpp.txt"
+res_test_java = "res_test_java.txt"
+
+PI = [0.2148, 0.3672, 0.2305, 0.1875]
